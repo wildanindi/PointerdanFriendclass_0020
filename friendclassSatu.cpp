84e9149170b91e491e9c28a5b132cff842c2d734
wildanindi <wildanindi2@gmail.com>
2024-05-07 18:33:18 +0700

    prosedur untuk menjalankan class

--- friendclassSatu.cpp
+++ friendclassSatu.cpp
@@ -22,3 +22,28 @@ class siswa
         void setId(int pId);
         void displayAll(orang &a);   
 };
+
+    void siswa::displayAll(orang &a)
+    {
+        cout << id << endl << a.nama;
+    }
+
+    void orang::setNama(string pNama)
+    {
+        nama = pNama;
+    }
+
+    void siswa::setId(int pId)
+    {
+        id = pId;
+    }
+
+    int main ()
+    {
+        orang joko;
+        joko.setNama("Joko Susilo");
+        siswa joko_siswa;
+        joko_siswa.setId(1);
+        joko_siswa.displayAll(joko);
+        return 0;
+    }
