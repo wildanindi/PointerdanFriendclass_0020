e76feebe00d4eff7a9c5049823632d58996c56ac
wildanindi <wildanindi2@gmail.com>
2024-05-07 17:40:44 +0700

    menambah objek menggunakan tanda & untuk class mahasiswa

--- pointer.cpp
+++ pointer.cpp
@@ -14,5 +14,9 @@ int main(){
     mahasiswa mhs{1}; //object mhs
     mhs.showNim(); // Member access operator
 
+    mahasiswa &refMhs = mhs; //Pointer reference refMhs
+    refMhs.nim = 2; //member access operator
+    mhs.showNim();
+
     
 }
